699f4245c8e8580bb9ba0c2df4d63141d6afc523
Rodrigo Peixoto <rodrigo.peixoto@outlook.com>
2024-05-25 18:17:24 -0300

    Add project files.

---
+++ ComercioEletronico/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ComercioEletronico.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ComercioEletronico.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
